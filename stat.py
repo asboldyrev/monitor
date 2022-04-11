0374f4f75aac22ec3951b3d4114ebbbef55d2513
asboldyrev <angelcorpc@ya.ru>
2022-04-11 22:13:05 +0700

    Переделал скрипт сбора статистики на новую кодовую базу

--- stat.py
+++ stat.py
@@ -1,9 +1,31 @@
 import json
-import src.cpu as cpu
+import python.cpu as cpu
+import python.system as system
+import python.memory as memory
+import python.average as average
+import python.filesystems as filesystems
+import python.docker as docker
 
 cpu_info = cpu.info()
-max_frequency = cpu.getMaxFrequency()
-cpu_info["frequency"] = max_frequency if max_frequency else cpu_info["frequency"]
+with open('./json/cpu.json', 'w') as file:
+    json.dump(cpu_info, file)
 
-with open('data.json', 'w') as file:
-    json.dump({'cpuInfo': cpu_info}, file)
+system_info = system.info()
+with open('./json/system.json', 'w') as file:
+    json.dump(system_info, file)
+
+memory_info = memory.info()
+with open('./json/memory.json', 'w') as file:
+    json.dump(memory_info, file)
+
+average_info = average.info()
+with open('./json/average.json', 'w') as file:
+    json.dump(average_info, file)
+
+filesystems_info = filesystems.info()
+with open('./json/filesystems.json', 'w') as file:
+    json.dump(filesystems_info, file)
+
+docker_info = docker.info()
+with open('./json/docker.json', 'w') as file:
+    json.dump(docker_info, file)
